6ab683beeed196e35218074f75403c3c9c85801f
SkyNeon22 <113529402+SkyNeon22@users.noreply.github.com>
2025-09-09 22:31:26 +0300

    Add files via upload

---
+++ configs/config.py
@@ -0,0 +1,16 @@
+#---------------------------------------------------------------------#
+#                       GAME CONFIG
+# Include the stuff like difficulties
+# Locations of the saves and assets
+# And other globals
+# A configuration file for the game
+#                       @2025 skyneon22
+#---------------------------------------------------------------------#
+# this is the basic built-in config
+import pygame as pg
+import random as rng
+
+difficulties = ("Easy","Normal","Hard","Lunatic","Extra")
+
+RES_EDITOR = (960, 786)
+RES = (640, 480)
