333e9d844d2a02c8fce2a571c1b5006a2905fc16
SkyNeon22 <113529402+SkyNeon22@users.noreply.github.com>
2025-10-27 22:57:42 +0200

    Delete core directory

--- core/low/__init__.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-
-
-def pginit():
-    pg.init()
-    pg.mixer.init()
--- core/saving.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-from core.game.registries import REGISTRY
-
-
-class SaveSystem(REGISTRY):
-    # TODO: Saving progress, spellcard captures, playtime, characters that have extra stage unlocked and other stats
-    pass
